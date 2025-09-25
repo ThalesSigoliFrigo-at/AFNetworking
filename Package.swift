f6ce96a146edd45a8c6f89bf46b542eb7c321d5d
Thales Sigoli Frigo <thales.sigolifrigo@airtable.com>
2025-09-25 10:37:37 -0300

    Create UIKit_AFTNetworking target

--- Package.swift
+++ Package.swift
@@ -30,8 +30,16 @@ let package = Package(name: "AFNetworking",
                                   .iOS(.v9),
                                   .tvOS(.v9),
                                   .watchOS(.v2)],
-                      products: [.library(name: "AFNetworking",
-                                          targets: ["AFNetworking"])],
-                      targets: [.target(name: "AFNetworking",
-                                        path: "AFNetworking",
-                                        publicHeadersPath: "")])
+                      products: [
+                        .library(name: "AFNetworking", targets: ["AFNetworking"]),
+                        .library(name: "UIKit_AFNetworking", targets: ["UIKit_AFNetworking"]) // UIKit extensions
+                      ],
+                      targets: [
+                        .target(name: "AFNetworking",
+                                path: "AFNetworking",
+                                publicHeadersPath: ""),
+                        .target(name: "UIKit_AFNetworking",
+                                dependencies: ["AFNetworking"],
+                                path: "UIKit+AFNetworking",
+                                publicHeadersPath: "")
+                      ])
